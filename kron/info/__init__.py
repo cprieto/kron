e232c3ea366bf42869f47523bc401e1aff4e8c80
Cristian Prieto <me@cprieto.com>
2018-10-03 13:19:38 +0200

    [Enhacement] Display timezone in job listing
     - Now we display the timezone associated with the job

--- kron/info/__init__.py
+++ kron/info/__init__.py
@@ -4,7 +4,7 @@ from terminaltables import SingleTable
 
 from ..kronbute import BaseServer
 
-__version__ = "1.11"
+__version__ = "1.12"
 
 
 @click.command(help='Show information about Kron and Kronbute')
--- kron/job/list.py
+++ kron/job/list.py
@@ -9,11 +9,11 @@ from ..kronbute import JobServer
 @click.pass_obj
 def list_jobs(server: JobServer):
     jobs = server.list()
-    data = [['Id', 'Alias', 'Name/Description', 'Schedule', 'Last Status', 'Last run', 'Next run']]
+    data = [['Id', 'Alias', 'Name/Description', 'Schedule', 'Last Status', 'Last run', 'Next run', 'Time Zone']]
     for job in jobs:
         data.append(
             [job['id'], util.format_none(job['alias'] if 'alias' in job else ''), job['name'], job['scheduleText'],
-             util.format_status(job['lastStatus']), job['lastRun'], job['nextRun']])
+             util.format_status(job['lastStatus']), job['lastRun'], job['nextRun'], job['timeZone']])
 
     table = AsciiTable(data)
     click.echo(table.table)
