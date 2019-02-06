fc5a73b68b2317c0f776e9e4c6680ff38fd23083
Obed Espinoza <obed.espinoza@healthcare.com>
2019-02-06 14:51:42 -0600

    Updating to 1.13 version

--- kron/info/__init__.py
+++ kron/info/__init__.py
@@ -4,7 +4,7 @@ from terminaltables import SingleTable
 
 from ..kronbute import BaseServer
 
-__version__ = "1.12"
+__version__ = "1.13"
 
 
 @click.command(help='Show information about Kron and Kronbute')
